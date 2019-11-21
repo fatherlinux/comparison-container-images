f6f89351b75cd2460cf35191e382955a7b5feed2
Scott McCarty <scott.mccarty@gmail.com>
2019-11-21 06:55:58 -0500

    SM: basic script to compare images

---
+++ run-analysis.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+images="ubi8/ubi ubi8/ubi-minimal fedora centos:7 debian:bullseye-slim debian:bullseye ubuntu:latest alpine:latest"
+
+get_version() {
+	echo Version: `podman run -it $1 cat /etc/os-release | grep PRETTY`
+}
+
+get_c_library() {
+	echo C Library: `podman run -it $1 ldd /bin/sh | grep libc`
+	}
+
+get_package_manager() {
+	echo Packager: `podman run -it $1 sh -c "ls /usr/bin /sbin | egrep '^rpm$|^apk$|^apt$'"`
+	}
+
+get_size() {
+	name=`echo $1 | md5sum | cut -f1 -d" "`
+	if [ ! -f $name ]
+	then
+		podman save -o $name $1 2>/dev/null
+	fi
+	if [ ! -f "$name.gz" ]
+	then
+		gzip --keep $name
+	fi
+	compressed_size=`du -sh $name.gz | cut -f1`
+	echo Compressed Size: $1: $compressed_size
+	size=`du -sh $name | cut -f1`
+	echo Size: $1: $size
+}
+
+cache_images() {
+	for i in $images
+	do
+		podman pull -q $i
+	done
+}
+
+#cache_images
+
+for i in $images
+do 
+	get_version $i
+	get_c_library $i
+	get_package_manager $i
+	get_size $i
+	echo ""
+done
+
+
+
+
